23a400bd2f1745dfbcc9d47c515a176cea8342be
dmitri <dmtrgrvnk@gmail.com>
2023-04-14 04:15:50 +0300

    added the ability to share posts via mail Google

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import (Paginator, EmptyPage,
                                    PageNotAnInteger)
+from django.core.mail import send_mail
 
 from .models import Post
+from .forms import EmailPostForm
 
 
 def post_list(request):
@@ -35,3 +37,30 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+
+def post_share(request, post_id):
+    """sending posts by mail"""
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == 'POST':
+        # validation form
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # clean form and send e-mail
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}'s comments: {cd['comments']}"
+            send_mail(subject, message, 'dmtrgrvnk@gmail.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html',
+                  {'post': post, 'form': form, 'sent': sent})
